bc0eb541690ebad2cf735243cc54fec8bc68ab0a
Utilka <andriy.v.bolshakov@gmail.com>
2022-02-08 11:11:39 +0200

    sln init

---
+++ Shadow-PUDGE/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿namespace Shadow_PUDGE.Models
+{
+    public class Products 
+    {
+        // ID книги
+        public int Id { get; set; }
+        //назва книги
+        public string Title { get; set; }
+        // автор книги
+        public string Description { get; set; }
+        // цiна
+        public int Price { get; set; }
+        public int PriceCompare { get; set; }
+        public int ProductsStatus { get; set; }
+    }
+}
