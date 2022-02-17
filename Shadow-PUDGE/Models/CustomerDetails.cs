ef45a0e34bc9d3626f68b249687b75c223788b0f
Utilka <andriy.v.bolshakov@gmail.com>
2022-02-17 12:32:05 +0200

    add customer details

---
+++ Shadow-PUDGE/Models/CustomerDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shadow_PUDGE.Models
+{
+    public class CustomerDetails
+    {
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<CustomerUser> manager)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //    // Add custom user claims here
+        //    return userIdentity;
+        //}
+
+        //Extended Properties
+        public DateTime? BirthDate { get; set; }
+        public long? OrganizationId { get; set; }
+
+        //Key Mappings
+        [ForeignKey("OrganizationId")]
+        public virtual Organization Organization { get; set; }
+    }
+}
