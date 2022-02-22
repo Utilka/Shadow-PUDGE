ab9c79b94f2e06b359eb7beae1f05d8a6850d6ef
storm <storminside4766@gmail.com>
2022-02-22 09:48:49 +0200

    fix id type

--- Shadow-PUDGE/Models/Order.cs
+++ Shadow-PUDGE/Models/Order.cs
@@ -11,7 +11,7 @@
         public DateTime CreatedAt { get; set; }
         public OrderStatus Status { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public CustomerDetails CustomerDetails { get; set; }
     }
 }
